34a74b41f05d8de04b8e4325f077baa18cc20748
Your Name <omjadhav963@gmail.com>
2021-11-25 17:19:52 +0530

    scroll top

--- js/custom.js
+++ js/custom.js
@@ -53,88 +53,88 @@ $(document).ready(function () {
 	/*	Animated Scroll To Anchor
 	/*----------------------------------------------------*/
 
-	// $('.header a[href^="#"], .page a.btn[href^="#"], .page a.internal-link[href^="#"]').on('click', function (e) {
+	$('.header a[href^="#"], .page a.btn[href^="#"], .page a.internal-link[href^="#"]').on('click', function (e) {
 
-	// 	e.preventDefault();
+		e.preventDefault();
 
-	// 	var target = this.hash,
-	// 		$target = jQuery(target);
+		var target = this.hash,
+			$target = jQuery(target);
 
-	// 	$('html, body').stop().animate({
-	// 		'scrollTop': $target.offset().top - 60 // - 200px (nav-height)
-	// 	}, 'slow', 'easeInSine', function () {
-	// 		window.location.hash = '1' + target;
-	// 	});
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top - 60 // - 200px (nav-height)
+		}, 'slow', 'easeInSine', function () {
+			window.location.hash = '1' + target;
+		});
 
-	// });
+	});
 
 
 	/*----------------------------------------------------*/
 	/*	ScrollUp
 	/*----------------------------------------------------*/
 
-	// $.scrollUp = function (options) {
+	$.scrollUp = function (options) {
 
-	// 	// Defaults
-	// 	var defaults = {
-	// 		scrollName: 'scrollUp', // Element ID
-	// 		topDistance: 600, // Distance from top before showing element (px)
-	// 		topSpeed: 800, // Speed back to top (ms)
-	// 		animation: 'fade', // Fade, slide, none
-	// 		animationInSpeed: 200, // Animation in speed (ms)
-	// 		animationOutSpeed: 200, // Animation out speed (ms)
-	// 		scrollText: '', // Text for element
-	// 		scrollImg: false, // Set true to use image
-	// 		activeOverlay: false // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-	// 	};
+		// Defaults
+		var defaults = {
+			scrollName: 'scrollUp', // Element ID
+			topDistance: 600, // Distance from top before showing element (px)
+			topSpeed: 800, // Speed back to top (ms)
+			animation: 'fade', // Fade, slide, none
+			animationInSpeed: 200, // Animation in speed (ms)
+			animationOutSpeed: 200, // Animation out speed (ms)
+			scrollText: '', // Text for element
+			scrollImg: false, // Set true to use image
+			activeOverlay: false // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+		};
 
-	// 	var o = $.extend({}, defaults, options),
-	// 		scrollId = '#' + o.scrollName;
+		var o = $.extend({}, defaults, options),
+			scrollId = '#' + o.scrollName;
 
-	// 	// Create element
-	// 	$('<a/>', {
-	// 		id: o.scrollName,
-	// 		href: '#top',
-	// 		title: o.scrollText
-	// 	}).appendTo('body');
+		// Create element
+		$('<a/>', {
+			id: o.scrollName,
+			href: '#top',
+			title: o.scrollText
+		}).appendTo('body');
 
-	// 	// If not using an image display text
-	// 	if (!o.scrollImg) {
-	// 		$(scrollId).text(o.scrollText);
-	// 	}
+		// If not using an image display text
+		if (!o.scrollImg) {
+			$(scrollId).text(o.scrollText);
+		}
 
-	// 	// Minium CSS to make the magic happen
-	// 	$(scrollId).css({ 'display': 'none', 'position': 'fixed', 'z-index': '99999', 'margin-bottom': '5%', });
+		// Minium CSS to make the magic happen
+		$(scrollId).css({ 'display': 'none', 'position': 'fixed', 'z-index': '99999', 'margin-bottom': '7%', });
 
-	// 	// Active point overlay
-	// 	if (o.activeOverlay) {
-	// 		$("body").append("<div id='" + o.scrollName + "-active'></div>");
-	// 		$(scrollId + "-active").css({ 'position': 'absolute', 'top': o.topDistance + 'px', 'width': '100%', 'border-top': '1px dotted ' + o.activeOverlay, 'z-index': '99999' });
-	// 	}
+		// Active point overlay
+		if (o.activeOverlay) {
+			$("body").append("<div id='" + o.scrollName + "-active'></div>");
+			$(scrollId + "-active").css({ 'position': 'absolute', 'top': o.topDistance + 'px', 'width': '100%', 'border-top': '1px dotted ' + o.activeOverlay, 'z-index': '99999' });
+		}
 
-	// 	// Scroll function
-	// 	$(window).on('scroll', function () {
-	// 		switch (o.animation) {
-	// 			case "fade":
-	// 				$(($(window).scrollTop() > o.topDistance) ? $(scrollId).fadeIn(o.animationInSpeed) : $(scrollId).fadeOut(o.animationOutSpeed));
-	// 				break;
-	// 			case "slide":
-	// 				$(($(window).scrollTop() > o.topDistance) ? $(scrollId).slideDown(o.animationInSpeed) : $(scrollId).slideUp(o.animationOutSpeed));
-	// 				break;
-	// 			default:
-	// 				$(($(window).scrollTop() > o.topDistance) ? $(scrollId).show(0) : $(scrollId).hide(0));
-	// 		}
-	// 	});
+		// Scroll function
+		$(window).on('scroll', function () {
+			switch (o.animation) {
+				case "fade":
+					$(($(window).scrollTop() > o.topDistance) ? $(scrollId).fadeIn(o.animationInSpeed) : $(scrollId).fadeOut(o.animationOutSpeed));
+					break;
+				case "slide":
+					$(($(window).scrollTop() > o.topDistance) ? $(scrollId).slideDown(o.animationInSpeed) : $(scrollId).slideUp(o.animationOutSpeed));
+					break;
+				default:
+					$(($(window).scrollTop() > o.topDistance) ? $(scrollId).show(0) : $(scrollId).hide(0));
+			}
+		});
 
-	// 	// To the top
-	// 	$(scrollId).on('click', function (event) {
-	// 		$('html, body').animate({ scrollTop: 0 }, o.topSpeed);
-	// 		event.preventDefault();
-	// 	});
+		// To the top
+		$(scrollId).on('click', function (event) {
+			$('html, body').animate({ scrollTop: 0 }, o.topSpeed);
+			event.preventDefault();
+		});
 
-	// };
+	};
 
-	// $.scrollUp();
+	$.scrollUp();
 
 
 	/*----------------------------------------------------*/
