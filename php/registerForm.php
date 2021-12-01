a6ef96c3d51f9edc911943ae8df6eb90f731d895
Your Name <omjadhav963@gmail.com>
2021-12-01 21:14:16 +0530

    index.html edit

--- php/registerForm.php
+++ php/registerForm.php
@@ -4,7 +4,8 @@ $myemail = 'mailbox4lw@gmail.com';
 if(empty($_POST['name'])  ||
    empty($_POST['email']) ||
    empty($_POST['phone']) ||
-   empty($_POST['course']))
+   empty($_POST['course']) ||
+empty($_POST['course-type']))
 {
     $errors .= "\n Error: all fields are required";
 }
@@ -12,6 +13,7 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $course = $_POST['course'];
+$course_type =$_POST['course-type'];
 if (!preg_match(
 "/ ^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
 $email_address))
@@ -20,7 +22,7 @@ $email_address))
 }
 $to_email = "mailbox4lw@gmail.com";
 $subject = "New Application for Registration";
-$body = "Email is : ".$email_address."\n"."Name is :".$name."\n"."Phone is : ".$phone. "\n"."Course is :".$course;
+$body = "Email is : ".$email_address."\n"."Name is :".$name."\n"."Phone is : ".$phone. "\n"."Course is :".$course ."\n"."Course Type is :".$course_type; 
 if (mail($to_email, $subject, $body)) {
     //echo "Email successfully sent to $to_email...";
  echo "<script>alert('Email successfully sent to $to_email');</script>";
@@ -28,18 +30,18 @@ if (mail($to_email, $subject, $body)) {
 else {
     echo "<script>alert('Email Not Exists...');</script>";
 
-    $email=$_GET['email'];
-    $name=$_GET['name'];
-    $phone=$_GET['phone'];
+    // $email=$_GET['email'];
+    // $name=$_GET['name'];
+    // $phone=$_GET['phone'];
 
-    $to_email = "mailbox4lw@gmail.com";
-    $subject = "Your New credentials for";
-    $body = "Your username is : ".$email."\n"."Your name is :".$name."\n"."Phone : ".$phone;
-    if (mail($to_email, $subject, $body)) {
-        //echo "Email successfully sent to $to_email...";
-        echo "<script>alert('Email successfully sent to $to_email');</script>";
-    } else {
-        echo "<script>alert('Email Not Exists...');</script>";
-    }
+    // $to_email = "mailbox4lw@gmail.com";
+    // $subject = "Your New credentials for";
+    // $body = "Your username is : ".$email."\n"."Your name is :".$name."\n"."Phone : ".$phone;
+    // if (mail($to_email, $subject, $body)) {
+    //     //echo "Email successfully sent to $to_email...";
+    //     echo "<script>alert('Email successfully sent to $to_email');</script>";
+    // } else {
+    //     echo "<script>alert('Email Not Exists...');</script>";
+    // }
 }
  ?>
