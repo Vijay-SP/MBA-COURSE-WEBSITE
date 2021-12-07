7b0e476bf27f6bd65e259949c6e2217d94ef8a44
Vijay-SP <vijayprajapati6350@gmail.com>
2021-12-07 20:24:04 +0530

    updates

--- php/send_sms.php
+++ php/send_sms.php
@@ -2,6 +2,9 @@
 $OTP = rand(00000,99999);
 session_start();
 $_SESSION['__OTP'] = $OTP;
+$_SESSION['__OTP'] = $OTP;
+$_SESSION['__OTP'] = $OTP;
+$_SESSION['__OTP'] = $OTP;
 $mobile_number = $_POST['phone'];
 $message_content = 'Your verification code for login is '.$OTP.'';
 $url = 'http://mysmsshop.in/http-api.php?username=agixinternational&password=Akash@936/*&senderid=ONEESM&route=1&number='.$mobile_number.'&message='.urlencode($message_content).''; 
@@ -11,38 +14,7 @@ function sendSMS($fields) {
 	curl_setopt( $res, CURLOPT_URL, $fields );
 	curl_setopt( $res, CURLOPT_RETURNTRANSFER, true ); 
 	$result = curl_exec( $res );
-	echo $result."<script>alert('OTP sent successfully')</script>";   
+    $newotp = base64_encode($_SESSION['__OTP']);
+	header("location:verify_sms.php?pwd=".$newotp);  
 } 
-
-$errors = '';
-$myemail = 'mailbox4lw@gmail.com';
-if(empty($_POST['name'])  ||
-   empty($_POST['email']) ||
-   empty($_POST['phone']) ||
-   empty($_POST['course']) ||
-empty($_POST['course-type']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$course = $_POST['course'];
-$course_type =$_POST['course-type'];
-if (!preg_match(
-"/ ^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-$to_email = "mailbox4lw@gmail.com";
-$subject = "New Application for Registration";
-$body = "Email is : ".$email_address."\n"."Name is :".$name."\n"."Phone is : ".$phone. "\n"."Course is :".$course ."\n"."Course Type is :".$course_type; 
-if (mail($to_email, $subject, $body)) {
-    //echo "Email successfully sent to $to_email...";
- echo "<script>alert('Email successfully sent to $to_email');</script>";
-} 
-else {
-    echo "<script>alert('Email Not Exists...');</script>";
-}
 ?>
--- php/verify_sms.php
+++ php/verify_sms.php
@@ -1,10 +1,42 @@
-<?php 
-session_start();
-$OTP = $_SESSION['__OTP'];
-$UserOTP = $_GET['otp'];
-if ($OTP == $UserOTP){
-    echo 1;
-}else{
-    echo 0;
-}
+<?php
+$otp = $_GET['pwd'];
+$otp = base64_decode($otp);
 ?>
+<html>
+<script
+  src="https://code.jquery.com/jquery-3.6.0.js"
+  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+  crossorigin="anonymous"></script>
+    <body>
+				<div class="col-md-5">
+					<div id="register-form">
+						<form method="POST" action=""  class="row register-form">
+
+							<!-- Form Input -->
+							<div id="input-name" class="col-md-12">
+								<p>OTP*</p>
+								<input type="text" name="otp" class="form-control name" id = "otp" placeholder="Enter Your Name*"
+									required>
+							</div>
+
+                            <div class="col-md-12 form-btn">
+								<button type="submit" value="done" name="done" id = "btn" class="btn btn-md btn-orange tra-black-hover submit">
+									SUBMIT
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+					<script>
+						$("#btn").on("click",function() {
+							var otp = $("#otp").val();
+							if(otp == <?php echo $otp; ?>) {
+								
+							}
+							else {
+								alert("Otp dont match");
+							}
+						});
+					</script>
+</body>
+</html>
