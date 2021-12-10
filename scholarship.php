f5e818bbee95c25ef06523c48e260a0b576e9bb9
Your Name <omjadhav963@gmail.com>
2021-12-10 20:49:14 +0530

    fixes

--- scholarship.php
+++ scholarship.php
@@ -3,15 +3,13 @@ $errors = '';
 $myemail = 'mailbox4lw@gmail.com';
 if(empty($_POST['name1'])  ||
    empty($_POST['email1']) ||
-   empty($_POST['phone1']) ||
-   empty($_POST['course1']))
+   empty($_POST['phone1']))
 {
     $errors .= "\n Error: all fields are required";
 }
 $name = $_POST['name1'];
 $email_address = $_POST['email1'];
 $phone = $_POST['phone1'];
-$course = $_POST['course1'];
 if (!preg_match(
 "/ ^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
 $email_address))
@@ -20,7 +18,7 @@ $email_address))
 }
 $to_email = "mailbox4lw@gmail.com";
 $subject = "New Application for Scholarship";
-$body = "Email is : ".$email_address."\n"."Name is :".$name."\n"."Phone is : ".$phone. "\n"."Course is :".$course;
+$body = "Email is : ".$email_address."\n"."Name is :".$name."\n"."Phone is : ".$phone. "\n";
 if (mail($to_email, $subject, $body)) {
     //echo "Email successfully sent to $to_email...";
  echo "<script>alert('Email successfully sent to $to_email');</script>";
@@ -29,4 +27,3 @@ else {
      echo "<script>alert('Email Not Exists...');</script>";
 
 }
-?>
